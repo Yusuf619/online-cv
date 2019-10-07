c7030e828cd860f110385eb85cfd79436b39bbd2
Yusuf <Your Actual Email>
2019-10-07 10:57:18 +0200

    added main structure

--- main.js
+++ main.js
@@ -1,40 +1,24 @@
-// function openNav() {
-//     document.getElementById("mySidenav").style.width = "250px";
-//   }
-  
-//   function closeNav() {
-//     document.getElementById("mySidenav").style.width = "0";
-//   }
-
-
-
-//   $(document).ready(function() {
- 
-//     $("#owl-example").owlCarousel();
-   
-//   });
-
-
-
-//   window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//     document.getElementById("navbar").style.padding = "30px 10px";
-//     document.getElementById("logo").style.fontSize = "25px";
-//   } else {
-//     document.getElementById("navbar").style.padding = "80px 10px";
-//     document.getElementById("logo").style.fontSize = "35px";
-//   }
-// }
-
-
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
+    var x = document.getElementById("mnavbar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
   }
-}
+    /* **************************************************************************************************************************/
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("mnavbar").style.top = "0";
+    } else {
+      document.getElementById("mnavbar").style.top = "-100px";
+    }
+  }
+
+
+
+
+
 
