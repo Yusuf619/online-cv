7b50865a865841ed09af9491a63d304e49445c26
Yusuf <Your Actual Email>
2019-10-08 09:02:51 +0200

    added responsive navbar

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
+// function myFunction() {
+//     var x = document.getElementById("mnavbar");
+//     if (x.className === "navbar") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "navbar";
+//     }
+//   }
+
 function myFunction() {
-    var x = document.getElementById("mnavbar");
-    if (x.className === "navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
+}
     /* **************************************************************************************************************************/
   window.onscroll = function() {scrollFunction()};
 
